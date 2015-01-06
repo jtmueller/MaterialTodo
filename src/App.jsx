d2f59cc6c20e3735c2383dec115b6507c2d87d55
Joel Mueller <joel.mueller@gmail.com>
2015-01-05 21:47:44 -0600

    Allowing gh-pages deployment folder.

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,26 @@
 
 var React = require('react');
 var Router = require('react-router');
-var Route = Router.Route;
+var {Route, Redirect, RouteHandler} = Router;
+var TodoApp = require('./components/TodoApp.jsx');
 
 // Export React so the dev tools can find it
 (window !== window.top ? window.top : window).React = React;
 
+var App = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <RouteHandler />
+      </div>
+    );
+  }
+});
+
 var routes = (
-  <Route name="app" path="/" handler={require('./layouts/Default.jsx')}>
-    <Route name="home" path="/" handler={require('./pages/Home.jsx')} />
-    <Route name="privacy" handler={require('./pages/Privacy.jsx')} />
+  <Route name="app" handler={App}>
+    <Route name="github" path="/MaterialTodo/" handler={TodoApp} />
+    <Route name="home" path="/" handler={TodoApp} />
   </Route>
 );
 
